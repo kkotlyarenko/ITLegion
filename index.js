f386a17b201af98b863772efe2a54df4efb89abd
kkotlyarenko <kirill07022006@mail.ru>
2021-12-17 22:22:22 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+server.listen(3000);
+
+app.get('/', function(request, respons) {
+	respons.sendFile(__dirname + '/index.html');
+});
+
+connections = [];
+
+io.sockets.on('connection', function(socket) {
+	console.log("Успешное соединение");
+	connections.push(socket);
+
+	socket.on('disconnect', function(data) {
+		connections.splice(connections.indexOf(socket), 1);
+		console.log("Отключились");
+	});
+
+});
